ProtonWallet/WebClients

commit e7100a8ab65e63cb31765b2b5779cce242d9f503

Treat unknown calendars as read-only
CALWEB-4399

packages/components/containers/calendar/calendarModal/CalendarModal.tsx
@@ -5,7 +5,7 @@ import { c } from 'ttag';
 import { Button } from '@proton/atoms';
 import { useContactEmailsCache } from '@proton/components/containers/contacts/ContactEmailsProvider';
 import { dedupeNotifications, sortNotificationsByAscendingTrigger } from '@proton/shared/lib/calendar/alarms';
-import { getIsSubscribedCalendar } from '@proton/shared/lib/calendar/calendar';
+import { getIsSubscribedCalendar, getShowDuration } from '@proton/shared/lib/calendar/calendar';
 import { MAX_CHARS_API, MAX_DEFAULT_NOTIFICATIONS } from '@proton/shared/lib/calendar/constants';
 import { getCalendarCreatedByText } from '@proton/shared/lib/calendar/sharing/shareProton/shareProton';
 import { Nullable } from '@proton/shared/lib/interfaces';
@@ -83,7 +83,7 @@ export const CalendarModal = ({
         return (option && option.text) || '';
     }, [model.addressID, model.addressOptions]);
 
-    const isSubscribedCalendar = initialCalendar && getIsSubscribedCalendar(initialCalendar);
+    const showDuration = initialCalendar && getShowDuration(initialCalendar);
     const subscribeURL =
         initialCalendar && getIsSubscribedCalendar(initialCalendar)
             ? initialCalendar.SubscriptionParameters.URL
@@ -244,7 +244,7 @@ export const CalendarModal = ({
         </InputFieldTwo>
     );
 
-    const defaultEventDurationRow = !isSubscribedCalendar ? (
+    const defaultEventDurationRow = showDuration ? (
         <InputFieldTwo
             as={SelectTwo}
             label={c('Label').t`Event duration`}

applications/calendar/src/app/components/eventModal/eventForm/state.ts
@@ -9,6 +9,7 @@ import {
     getIsCalendarWritable,
     getIsOwnedCalendar,
     getIsSubscribedCalendar,
+    getIsUnknownCalendar,
 } from '@proton/shared/lib/calendar/calendar';
 import {
     DAILY_TYPE,
@@ -131,6 +132,7 @@ const getCalendarsModel = (Calendar: VisualCalendar, Calendars: VisualCalendar[]
             isSubscribed: getIsSubscribedCalendar(calendar),
             isOwned: getIsOwnedCalendar(calendar),
             isWritable: getIsCalendarWritable(calendar),
+            isUnknown: getIsUnknownCalendar(calendar),
         })),
         calendar: {
             id: Calendar.ID,
@@ -139,6 +141,7 @@ const getCalendarsModel = (Calendar: VisualCalendar, Calendars: VisualCalendar[]
             isSubscribed: getIsSubscribedCalendar(Calendar),
             isOwned: getIsOwnedCalendar(Calendar),
             isWritable: getIsCalendarWritable(Calendar),
+            isUnknown: getIsUnknownCalendar(Calendar),
         },
     };
 };

applications/calendar/src/app/components/eventModal/inputs/CreateEventCalendarSelect.tsx
@@ -43,7 +43,7 @@ const CreateEventCalendarSelect = ({
                     isInvitation: !!organizer,
                 })
         )
-        .map(({ value, text, color, permissions, isOwned, isSubscribed, isWritable }) => ({
+        .map(({ value, text, color, permissions, isOwned, isSubscribed, isWritable, isUnknown }) => ({
             id: value,
             name: text,
             color,
@@ -51,6 +51,7 @@ const CreateEventCalendarSelect = ({
             isOwned,
             isSubscribed,
             isWritable,
+            isUnknown,
         }));
     const { name } = options.find(({ id }) => id === calendarID) || options[0];
 
@@ -65,7 +66,7 @@ const CreateEventCalendarSelect = ({
     }
 
     const handleChangeCalendar = async (newId: string) => {
-        const { color, permissions, isOwned, isSubscribed, isWritable } =
+        const { color, permissions, isOwned, isSubscribed, isWritable, isUnknown } =
             options.find(({ id }) => id === newId) || options[0];
 
         // grab members and default settings for the new calendar
@@ -98,7 +99,7 @@ const CreateEventCalendarSelect = ({
 
         setModel({
             ...model,
-            calendar: { id: newId, color, permissions, isOwned, isSubscribed, isWritable },
+            calendar: { id: newId, color, permissions, isOwned, isSubscribed, isWritable, isUnknown },
             ...getInitialMemberModel(addresses, Members, Member, address),
             ...getOrganizerAndSelfAddressModel({ attendees: model.attendees, addresses, addressID: address.ID }),
             defaultEventDuration,

applications/calendar/src/app/components/events/EventPopover.tsx
@@ -22,6 +22,7 @@ import {
     getIsCalendarWritable,
     getIsOwnedCalendar,
     getIsSubscribedCalendar,
+    getIsUnknownCalendar,
 } from '@proton/shared/lib/calendar/calendar';
 import { ICAL_ATTENDEE_STATUS, VIEWS } from '@proton/shared/lib/calendar/constants';
 import { getLinkToCalendarEvent, naiveGetIsDecryptionError } from '@proton/shared/lib/calendar/helper';
@@ -99,6 +100,7 @@ const EventPopover = ({
     const isCalendarDisabled = getIsCalendarDisabled(calendarData);
     const isSubscribedCalendar = getIsSubscribedCalendar(calendarData);
     const isOwnedCalendar = getIsOwnedCalendar(calendarData);
+    const isUnknownCalendar = getIsUnknownCalendar(calendarData);
     const isCalendarWritable = getIsCalendarWritable(calendarData);
 
     const recurrenceDate = toLocalDate(fromUTCDate(start));
@@ -180,7 +182,7 @@ const EventPopover = ({
     const reloadText = c('Reload event button tooltip').t`Reload event`;
     const viewText = c('View event button tooltip').t`Open in a new tab`;
 
-    const editButton = getCanEditEvent({ isCalendarDisabled }) && (
+    const editButton = getCanEditEvent({ isUnknownCalendar, isCalendarDisabled }) && (
         <Tooltip title={editText}>
             <ButtonLike
                 data-testid="event-popover:edit"
@@ -210,6 +212,7 @@ const EventPopover = ({
     );
     const duplicateButton = onDuplicate &&
         getCanDuplicateEvent({
+            isUnknownCalendar,
             isSubscribedCalendar,
             isOwnedCalendar,
             isOrganizer: model.isOrganizer,

applications/calendar/src/app/helpers/event.test.ts
@@ -82,19 +82,21 @@ describe('getCannotSaveEvent()', () => {
 });
 
 describe('getCanEditEvent()', () => {
-    test('User can edit events as long as the calendar is not disabled', () => {
+    test('User can edit events only if the calendar is of known type and is not disabled', () => {
         const combinations = [];
-        for (let i = 0; i < 1 ** 2; i++) {
-            const isCalendarDisabled = !!(1 & i);
+        for (let i = 0; i < 2 ** 2; i++) {
+            const isUnknownCalendar = !!(1 & i);
+            const isCalendarDisabled = !!(2 & i);
 
-            combinations.push({ isCalendarDisabled });
+            combinations.push({ isUnknownCalendar, isCalendarDisabled });
         }
-        combinations.forEach(({ isCalendarDisabled }) => {
+        combinations.forEach(({ isUnknownCalendar, isCalendarDisabled }) => {
             expect(
                 getCanEditEvent({
+                    isUnknownCalendar,
                     isCalendarDisabled,
                 })
-            ).toEqual(!isCalendarDisabled);
+            ).toEqual(!isCalendarDisabled && !isUnknownCalendar);
         });
     });
 });
@@ -190,7 +192,7 @@ describe('getCanEditSharedEventData()', () => {
         ).toEqual(false);
     });
 
-    test('User cannot edit shared event data in subscribed calendars', () => {
+    test('User cannot edit shared event data in subscribed calendars or other read-only calendars', () => {
         const combinations = [];
         for (let i = 0; i < 2 ** 3; i++) {
             const isOrganizer = !!(1 & i);
@@ -446,7 +448,7 @@ describe('getCanChangeCalendar()', () => {
 });
 
 describe('getIsAvailableCalendar()', () => {
-    test('User cannot change calendar of events in subscribed or other read-only calendars', () => {
+    test('User cannot change calendar of events to subscribed or other read-only calendars', () => {
         const combinations = [];
         for (let i = 0; i < 2 ** 2; i++) {
             const isOwnedCalendar = !!(1 & i);
@@ -505,6 +507,29 @@ describe('getIsAvailableCalendar()', () => {
 });
 
 describe('getCanDuplicateEvent()', () => {
+    test('User cannot duplicate events in unknown calendars', () => {
+        const combinations = [];
+        for (let i = 0; i < 2 ** 4; i++) {
+            const isSubscribedCalendar = !!(1 & i);
+            const isOwnedCalendar = !!(2 & i);
+            const isOrganizer = !!(4 & i);
+            const isInvitation = !!(8 & i);
+
+            combinations.push({ isSubscribedCalendar, isOwnedCalendar, isOrganizer, isInvitation });
+        }
+        combinations.forEach(({ isSubscribedCalendar, isOwnedCalendar, isOrganizer, isInvitation }) => {
+            expect(
+                getCanDuplicateEvent({
+                    isUnknownCalendar: true,
+                    isSubscribedCalendar,
+                    isOwnedCalendar,
+                    isInvitation,
+                    isOrganizer,
+                })
+            ).toEqual(false);
+        });
+    });
+
     test('User cannot duplicate events in subscribed calendars', () => {
         const combinations = [];
         for (let i = 0; i < 2 ** 2; i++) {
@@ -516,6 +541,7 @@ describe('getCanDuplicateEvent()', () => {
         combinations.forEach(({ isOrganizer, isInvitation }) => {
             expect(
                 getCanDuplicateEvent({
+                    isUnknownCalendar: false,
                     isSubscribedCalendar: true,
                     isOwnedCalendar: true,
                     isInvitation,
@@ -528,6 +554,7 @@ describe('getCanDuplicateEvent()', () => {
     test('Owner can duplicate events that are not invitations', () => {
         expect(
             getCanDuplicateEvent({
+                isUnknownCalendar: false,
                 isSubscribedCalendar: false,
                 isOwnedCalendar: true,
                 isInvitation: false,
@@ -539,6 +566,7 @@ describe('getCanDuplicateEvent()', () => {
     test('Owner can duplicate invitations that she is organizing', () => {
         expect(
             getCanDuplicateEvent({
+                isUnknownCalendar: false,
                 isSubscribedCalendar: false,
                 isOwnedCalendar: true,
                 isInvitation: true,
@@ -550,6 +578,7 @@ describe('getCanDuplicateEvent()', () => {
     test('Owner cannot duplicate invitations if she is not the organizer', () => {
         expect(
             getCanDuplicateEvent({
+                isUnknownCalendar: false,
                 isSubscribedCalendar: false,
                 isOwnedCalendar: true,
                 isInvitation: true,
@@ -561,6 +590,7 @@ describe('getCanDuplicateEvent()', () => {
     test('Member can duplicate events that are not invitations', () => {
         expect(
             getCanDuplicateEvent({
+                isUnknownCalendar: false,
                 isSubscribedCalendar: false,
                 isOwnedCalendar: false,
                 isInvitation: false,
@@ -579,6 +609,7 @@ describe('getCanDuplicateEvent()', () => {
         combinations.forEach(({ isOrganizer }) => {
             expect(
                 getCanDuplicateEvent({
+                    isUnknownCalendar: false,
                     isSubscribedCalendar: false,
                     isOwnedCalendar: false,
                     isInvitation: true,

applications/calendar/src/app/helpers/event.ts
@@ -24,8 +24,20 @@ export const getEventStatusTraits = (model: EventModelReadView) => {
     };
 };
 
-export const getCanEditEvent = ({ isCalendarDisabled }: { isCalendarDisabled: boolean }) => {
-    return !isCalendarDisabled;
+export const getCanEditEvent = ({
+    isUnknownCalendar,
+    isCalendarDisabled,
+}: {
+    isUnknownCalendar: boolean;
+    isCalendarDisabled: boolean;
+}) => {
+    if (isUnknownCalendar) {
+        return false;
+    }
+    if (isCalendarDisabled) {
+        return false;
+    }
+    return true;
 };
 
 export const getCanDeleteEvent = ({
@@ -47,16 +59,21 @@ export const getCanDeleteEvent = ({
 };
 
 export const getCanDuplicateEvent = ({
+    isUnknownCalendar,
     isSubscribedCalendar,
     isOwnedCalendar,
     isOrganizer,
     isInvitation,
 }: {
+    isUnknownCalendar: boolean;
     isSubscribedCalendar: boolean;
     isOwnedCalendar: boolean;
     isOrganizer: boolean;
     isInvitation: boolean;
 }) => {
+    if (isUnknownCalendar) {
+        return false;
+    }
     if (isSubscribedCalendar) {
         return false;
     }

packages/components/containers/calendar/settings/CalendarEventDefaultsSection.tsx
@@ -14,7 +14,7 @@ import { useApi, useLoading, useNotifications } from '@proton/components/hooks';
 import { updateCalendarSettings } from '@proton/shared/lib/api/calendars';
 import { dedupeNotifications, sortNotificationsByAscendingTrigger } from '@proton/shared/lib/calendar/alarms';
 import { modelToNotifications } from '@proton/shared/lib/calendar/alarms/modelToNotifications';
-import { getIsCalendarWritable, getIsHolidaysCalendar, getIsOwnedCalendar } from '@proton/shared/lib/calendar/calendar';
+import { getIsHolidaysCalendar, getShowDuration } from '@proton/shared/lib/calendar/calendar';
 import { MAX_DEFAULT_NOTIFICATIONS } from '@proton/shared/lib/calendar/constants';
 import {
     CalendarBootstrap,
@@ -47,7 +47,7 @@ const CalendarEventDefaultsSection = ({ calendar, bootstrap, canEdit }: Props) =
     const [loadingSavePartDayNotifications, withLoadingSavePartDayNotifications] = useLoading();
     const [loadingSaveFullDayNotifications, withLoadingSaveFullDayNotifications] = useLoading();
 
-    const showDuration = getIsCalendarWritable(calendar) && getIsOwnedCalendar(calendar);
+    const showDuration = getShowDuration(calendar);
     const cannotEdit = !canEdit;
     const isHolidaysCalendar = getIsHolidaysCalendar(calendar);
 

packages/components/containers/calendar/settings/CalendarSubpage.tsx
@@ -13,7 +13,7 @@ import {
 import CalendarSettingsBreadcrumbs from '@proton/components/containers/calendar/settings/CalendarSettingsBreadcrumbs';
 import { useApi, useGetCalendarBootstrap, useNotifications } from '@proton/components/hooks';
 import { getAllMembers, getCalendarInvitations } from '@proton/shared/lib/api/calendars';
-import { getIsOwnedCalendar, getIsSubscribedCalendar } from '@proton/shared/lib/calendar/calendar';
+import { getIsOwnedCalendar, getIsPersonalCalendar } from '@proton/shared/lib/calendar/calendar';
 import { MEMBER_PERMISSIONS } from '@proton/shared/lib/calendar/permissions';
 import { getCalendarsSettingsPath } from '@proton/shared/lib/calendar/settingsRoutes';
 import { Address, UserModel } from '@proton/shared/lib/interfaces';
@@ -153,7 +153,7 @@ const CalendarSubpage = ({
 
     const hasMembersOrInvitations = !!(members.length || invitations.length);
     const isOwner = getIsOwnedCalendar(calendar);
-    const isSubscribedCalendar = getIsSubscribedCalendar(calendar);
+    const isPersonalCalendar = getIsPersonalCalendar(calendar);
 
     const reRender = () => setRenderCount((count) => count + 1);
 
@@ -177,7 +177,7 @@ const CalendarSubpage = ({
                     bootstrap={bootstrap}
                     canEdit={user.hasNonDelinquentScope}
                 />
-                {isOwner && !isSubscribedCalendar && (
+                {isOwner && isPersonalCalendar && (
                     <CalendarShareSection
                         calendar={calendar}
                         addresses={addresses}

commit 33a45ce0c613d2306634121f1f264f1f7c048fd3

Fix broken event duration condition in calendar modal
The condition got broken with commit e7100a8ab65e63cb31765b2b5779cce242d9f503

CALWEB-4432

packages/components/containers/calendar/calendarModal/CalendarModal.tsx
@@ -83,7 +83,7 @@ export const CalendarModal = ({
         return (option && option.text) || '';
     }, [model.addressID, model.addressOptions]);
 
-    const showDuration = initialCalendar && getShowDuration(initialCalendar);
+    const showDuration = initialCalendar ? getShowDuration(initialCalendar) : true;
     const subscribeURL =
         initialCalendar && getIsSubscribedCalendar(initialCalendar)
             ? initialCalendar.SubscriptionParameters.URL